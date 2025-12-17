eeeb42f81e39a7a836468c947712eb47ea38bfa3
DendiNuanu <dendi@nuanu.com>
2025-12-17 12:43:58 +0800

    fix: type job param to any to fix build error

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -57,7 +57,7 @@ export default async function CareersPage() {
 
                     {jobs && jobs.length > 0 ? (
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                            {jobs.map((job) => (
+                            {jobs.map((job: any) => (
                                 <JobCard key={job.id} job={job} />
                             ))}
                         </div>
